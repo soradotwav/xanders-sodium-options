8f8c7c9bd312028dbc7e11a17936fe75a795e2c4
soradotwav <dev@soradotwav.com>
2026-02-04 05:11:07 +0100

    [fix] ensure that MoreCulling reset button actually exists and is rendered.

--- src/main/java/dev/isxander/xso/compat/MoreCullingCompat.java
+++ src/main/java/dev/isxander/xso/compat/MoreCullingCompat.java
@@ -3,17 +3,18 @@ package dev.isxander.xso.compat;
 import ca.fxco.moreculling.utils.CacheUtils;
 import dev.isxander.yacl3.api.ButtonOption;
 import dev.isxander.yacl3.api.ConfigCategory;
+import dev.isxander.yacl3.api.OptionDescription;
 import net.caffeinemc.mods.sodium.client.config.structure.OptionPage;
 import net.minecraft.text.Text;
 
 public class MoreCullingCompat {
 
     public static void extendMoreCullingPage(OptionPage page, ConfigCategory.Builder builder) {
-        if (page.name()
-                .getString()
-                .equals(Text.translatable("moreculling.title").getString())) {
+        if (page.name().equals(Text.translatable("moreculling.config.category.general"))) {
             builder.option(ButtonOption.createBuilder()
                     .name(Text.translatable("moreculling.config.resetCache"))
+                    .description(OptionDescription.of(Text.literal(
+                            "Clears More Culling's internal cache. Useful if you experience visual glitches after changing settings.")))
                     .action((screen, button) -> CacheUtils.resetAllCache())
                     .build());
         }
