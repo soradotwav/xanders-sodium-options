db99f211a1d68eb4aae1cf51776a71a243a9aa18
soradotwav <dev@soradotwav.com>
2026-02-06 07:14:37 +0100

    [fix] optimized hacky IO reset with faster hashmap to avoid stutters

--- src/main/java/dev/isxander/xso/SodiumBinding.java
+++ src/main/java/dev/isxander/xso/SodiumBinding.java
@@ -7,7 +7,12 @@ import net.caffeinemc.mods.sodium.client.gui.options.Option;
 import net.caffeinemc.mods.sodium.client.gui.options.binding.OptionBinding;
 import net.caffeinemc.mods.sodium.client.gui.options.storage.OptionStorage;
 
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
 public class SodiumBinding<S, T> implements Binding<T> {
+    private static final Map<Class<?>, Object> DEFAULT_DATA_CACHE = new ConcurrentHashMap<>();
+
     private final OptionBinding<S, T> sodiumBinding;
     private final OptionStorage<S> sodiumStorage;
 
@@ -39,41 +44,50 @@ public class SodiumBinding<S, T> implements Binding<T> {
             if (currentData != null) {
                 Class<?> storageClass = currentData.getClass();
 
-                try {
-                    java.lang.reflect.Method defaultsMethod = storageClass.getMethod("defaults");
-                    if (storageClass.isAssignableFrom(defaultsMethod.getReturnType())) {
-                        S defaultData = (S) defaultsMethod.invoke(null);
-                        return sodiumBinding.getValue(defaultData);
-                    }
-                } catch (Exception e) {
-                    /* ignore */ }
-
-                try {
-                    java.lang.reflect.Constructor<?> constructor = storageClass.getDeclaredConstructor();
-                    constructor.setAccessible(true);
-                    S defaultData = (S) constructor.newInstance();
+                S defaultData = (S) DEFAULT_DATA_CACHE.computeIfAbsent(storageClass,
+                        SodiumBinding::createDefaultInstance);
+                if (defaultData != null) {
                     return sodiumBinding.getValue(defaultData);
-                } catch (Exception e) {
-                    /* ignore */ }
-
-                try {
-                    java.lang.reflect.Constructor<?> constructor = storageClass
-                            .getDeclaredConstructor(net.minecraft.client.MinecraftClient.class, java.io.File.class);
-                    constructor.setAccessible(true);
-
-                    java.io.File tempFile = java.io.File.createTempFile("xso_defaults", ".txt");
-                    tempFile.delete();
-
-                    S defaultData = (S) constructor.newInstance(net.minecraft.client.MinecraftClient.getInstance(),
-                            tempFile);
-                    return sodiumBinding.getValue(defaultData);
-                } catch (Exception e) {
-                    /* ignore */
                 }
             }
         } catch (Exception e) {
             // ignore
         }
         return getValue();
+    }
+
+    @SuppressWarnings("unchecked")
+    private static <S> S createDefaultInstance(Class<?> storageClass) {
+        try {
+            java.lang.reflect.Method defaultsMethod = storageClass.getMethod("defaults");
+            if (storageClass.isAssignableFrom(defaultsMethod.getReturnType())) {
+                return (S) defaultsMethod.invoke(null);
+            }
+        } catch (Exception e) {
+            /* ignore */
+        }
+
+        try {
+            java.lang.reflect.Constructor<?> constructor = storageClass.getDeclaredConstructor();
+            constructor.setAccessible(true);
+            return (S) constructor.newInstance();
+        } catch (Exception e) {
+            /* ignore */
+        }
+
+        try {
+            java.lang.reflect.Constructor<?> constructor = storageClass
+                    .getDeclaredConstructor(net.minecraft.client.MinecraftClient.class, java.io.File.class);
+            constructor.setAccessible(true);
+
+            java.io.File tempFile = java.io.File.createTempFile("xso_defaults", ".txt");
+            tempFile.delete();
+
+            return (S) constructor.newInstance(net.minecraft.client.MinecraftClient.getInstance(), tempFile);
+        } catch (Exception e) {
+            /* ignore */
+        }
+
+        return null;
     }
 }
