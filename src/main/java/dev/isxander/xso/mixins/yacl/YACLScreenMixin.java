755929e63f6f11031951479a8147994d38bcfd63
soradotwav <dev@soradotwav.com>
2026-02-19 22:20:34 -0800

    [feat] allow for keyboard navigation

--- src/main/java/dev/isxander/xso/mixins/yacl/YACLScreenMixin.java
+++ src/main/java/dev/isxander/xso/mixins/yacl/YACLScreenMixin.java
@@ -5,10 +5,13 @@ import dev.isxander.yacl3.gui.YACLScreen;
 import java.util.List;
 import net.minecraft.client.gui.components.Button;
 import net.minecraft.client.gui.components.EditBox;
+import net.minecraft.client.gui.components.TabButton;
 import net.minecraft.client.gui.components.tabs.Tab;
 import net.minecraft.client.gui.components.tabs.TabManager;
 import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.client.input.KeyEvent;
 import net.minecraft.network.chat.Component;
+import org.jspecify.annotations.NonNull;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -76,5 +79,42 @@ public abstract class YACLScreenMixin extends Screen {
         }
         this.xso$donationButton.visible = isFirstTab;
         this.xso$donationButton.active = isFirstTab;
+    }
+
+    @Override
+    public boolean keyPressed(@NonNull KeyEvent keyEvent) {
+        if (this.tabNavigationBar != null
+                && keyEvent.hasControlDownWithQuirk()
+                && this.tabNavigationBar.keyPressed(keyEvent)) {
+            this.xso$ensureSelectedTabVisible();
+            return true;
+        }
+
+        return super.keyPressed(keyEvent);
+    }
+
+    @Unique
+    private void xso$ensureSelectedTabVisible() {
+        final int navMargin = 28;
+
+        int selectedIndex = this.tabNavigationBar.getTabs().indexOf(this.tabManager.getCurrentTab());
+        if (selectedIndex < 0) return;
+
+        var children = this.tabNavigationBar.children();
+        if (selectedIndex >= children.size()) return;
+
+        if (!(children.get(selectedIndex) instanceof TabButton tabButton)) return;
+
+        int left = tabButton.getX();
+        int right = left + tabButton.getWidth();
+        int visibleRight = this.width - navMargin;
+
+        if (left < navMargin) {
+            this.tabNavigationBar.setScrollOffset(
+                    this.tabNavigationBar.getScrollOffset() - (navMargin - left));
+        } else if (right > visibleRight) {
+            this.tabNavigationBar.setScrollOffset(
+                    this.tabNavigationBar.getScrollOffset() + (right - visibleRight));
+        }
     }
 }
